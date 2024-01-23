617d0bf17f4b1b34c9445545d2d539ab8154ab11
JhK <readytostart94@gmail.com>
2024-01-23 15:16:58 +0900

    New commit

--- src/NEw/Hello.java
+++ src/NEw/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Neww111");
         System.out.println("dffsf");
+        System.out.println("asdff");
     }
 }
